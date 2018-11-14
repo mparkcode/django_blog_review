7c623dc73155570bb74855dc5c53750990f997b4
Michael Park <mparkcode@gmail.com>
2018-11-14 12:37:34 +0000

    allowed hosts

--- django_blog/settings/base.py
+++ django_blog/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-review-mparkcode.c9users.io']
+ALLOWED_HOSTS = ['django-blog-review-mparkcode.c9users.io', 'mpark-django-blog-review.herokuapp.com']
 
 
 # Application definition
